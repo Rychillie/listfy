11de2b1d1b5f4102e71dd3fdabdefbce5525967c
Rychillie <rychillie@hotmail.com>
2022-08-17 20:26:30 -0300

    👌 IMPROVE: Detect if PWA is installed

---
+++ src/components/Welcome/index.tsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is the main page of the app. You can add more pages here.</p>
+    </div>
+  );
+}
